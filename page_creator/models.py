26ec7de59bffba1c9ec5096e70375e333cde3719
small4ek <genasmetchik@gmail.com>
2019-10-29 20:15:32 +0200

    start create unittests for PageCreator

--- page_creator/models.py
+++ page_creator/models.py
@@ -1,14 +1,9 @@
 from django.db import models
-from django.utils import timezone
 
 
 class PageCreator(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
 
     def __str__(self):
         return self.title
--- page_creator/tests.py
+++ page_creator/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .form import PageCreatorForm
 
-# Create your tests here.
+
+class PageCreatorFormTest(TestCase):
+
+    def test_page_creator_form_title_exist(self):
+        form = PageCreatorForm()
+        self.assertTrue(form['title'])
+
+    def test_page_creator_form_title_len(self):
+        form = PageCreatorForm()
+        self.assertTrue()
+        pass
+
