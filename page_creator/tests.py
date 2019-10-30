18e3c835d69daf66ec712439a19173b5beefa66b
small4ek <genasmetchik@gmail.com>
2019-10-30 11:22:40 +0200

    fix PageCreatorForm tests, working version

--- page_creator/tests.py
+++ page_creator/tests.py
@@ -6,8 +6,6 @@ from django.contrib.auth.models import AnonymousUser, User
 
 class PageCreatorFormTest(TestCase):
 
-    # TODO Form Test - POST request
-
     def test_page_creator_form_title_exist(self):
         form = PageCreatorForm()
         self.assertTrue(form['title'])
@@ -17,26 +15,28 @@ class PageCreatorFormTest(TestCase):
         for i in range(104):  # equal len = 202
             two_hundred_simbols = '{}{}'.format(two_hundred_simbols, str(i))
         data = {'title': two_hundred_simbols}
-        form = PageCreatorForm(data=data)
-        self.assertTrue(form.is_valid())
+        request = self.client.post('/page-creator/', data=data)
+        self.assertNotEqual(request.status_code, 302)
 
     def test_page_creator_form_title_default_len(self):
         two_hundred_simbols = ''
         for i in range(103):  # equal len = 199
             two_hundred_simbols = '{}{}'.format(two_hundred_simbols, str(i))
         data = {'title': two_hundred_simbols}
-        form = PageCreatorForm(data=data)
-        self.assertFalse(form.is_valid())
+        request = self.client.post('/page-creator/', data=data)
+        self.assertEqual(request.status_code, 302)
 
     def test_form_one_word_input(self):
         one_word = 'oneWord'
-        form = PageCreatorForm(data=one_word)
-        self.assertTrue(form.is_valid())
+        data = {'title': one_word}
+        request = self.client.post('/page-creator/', data=data)
+        self.assertEqual(request.status_code, 302)
 
     def test_form_more_words_input(self):
         two_words = 'two words'
-        form = PageCreatorForm(data=two_words)
-        self.assertFalse(form.is_valid())
+        data = {'title': two_words}
+        request = self.client.post('/page-creator/', data=data)
+        self.assertEqual(request.url, '/page-creator/input_error')
 
 
 class PageCreatorPageTest(TestCase):
